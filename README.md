5e3243506778fe20de2230ed81d7d5248edb933d
Nerjal Nosk <nosk.nerjal@gmx.com>
2023-05-19 23:38:21 +0200

    Update to 1.19.3

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A small (server-side) mod adding one command and one method to remove a set structure from your world.
 
+[<img alt="Requires Fabric API" src="https://i.imgur.com/Ol1Tcf8.png" width="30%">](https://modrinth.com/mod/fabric-api)
+
 :warning: This mod doesn't prevent the structures from being generated, but rather unregisters a set structure from your world.
 
 You're a server administrator, tired to have some mobs spawn at a specific place because of a structure is registered there? You're a mod creator wanting to have your structure be unregistered right after its generation? You're a modpack creator who wishes to unregister some structures as well?<br>
--- src/main/java/com/nerjal/structureremover/StructureRemoverCommand.java
+++ src/main/java/com/nerjal/structureremover/StructureRemoverCommand.java
@@ -5,20 +5,25 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
-import it.unimi.dsi.fastutil.longs.LongSet;
+import com.mojang.datafixers.util.Either;
 import net.minecraft.command.argument.RegistryPredicateArgumentType;
 import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntryList;
 import net.minecraft.world.gen.structure.Structure;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.command.argument.RegistryPredicateArgumentType.registryPredicate;
@@ -32,7 +37,7 @@ public class StructureRemoverCommand {
         LiteralArgumentBuilder<ServerCommandSource> r = LiteralArgumentBuilder.literal("struct-rem");
         dispatcher.register(r
                 .requires(s -> s.hasPermissionLevel(2))
-                .then(argument("structure", registryPredicate(Registry.STRUCTURE_KEY))
+                .then(argument("structure", registryPredicate(RegistryKeys.STRUCTURE))
                         .executes(StructureRemoverCommand::remove)
                         .then(argument("location", Vec3ArgumentType.vec3())
                                 .executes(StructureRemoverCommand::remove)
@@ -42,17 +47,23 @@ public class StructureRemoverCommand {
     }
 
     private static Optional<? extends RegistryEntryList.ListBacked<Structure>> getStructureListForPredicate(
-            RegistryPredicateArgumentType.RegistryPredicate<Structure> predicate, Registry<Structure> structureRegistry) {
-        return predicate.getKey()
-                .map(key -> structureRegistry.getEntry(key)
-                        .map(RegistryEntryList::of), structureRegistry::getEntryList);
+            RegistryPredicateArgumentType.@NotNull RegistryPredicate<Structure> predicate,
+            Registry<Structure> structureRegistry) {
+        Either<RegistryKey<Structure>, TagKey<Structure>> either = predicate.getKey();
+        Function<RegistryKey<Structure>, Optional<RegistryEntryList.Direct<?>>> var10001 = key ->
+                structureRegistry.getEntry(key).map(entry ->
+                        RegistryEntryList.of(new RegistryEntry[]{entry})
+            );
+        Objects.requireNonNull(structureRegistry);
+        return (Optional<RegistryEntryList.ListBacked<Structure>>)either.map(var10001, structureRegistry::getEntryList);
     }
 
     static int remove(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
         try {
-            Registry<Structure> registry = context.getSource().getWorld().getRegistryManager().get(Registry.STRUCTURE_KEY);
+            Registry<Structure> registry =
+                    context.getSource().getWorld().getRegistryManager().get(RegistryKeys.STRUCTURE);
             RegistryPredicateArgumentType.RegistryPredicate<Structure> predicate =
-                    getPredicate(context, "structure", Registry.STRUCTURE_KEY, STRUCTURE_INVALID_EXCEPTION);
+                    getPredicate(context, "structure", RegistryKeys.STRUCTURE, STRUCTURE_INVALID_EXCEPTION);
             RegistryEntryList<Structure> list = getStructureListForPredicate(predicate, registry)
                     .orElseThrow(() -> STRUCTURE_INVALID_EXCEPTION.create(predicate.asString()));
             Vec3d vec;
