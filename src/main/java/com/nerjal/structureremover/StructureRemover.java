e69ab8db9aafa1faca798f6cd7d814861078f274
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-10-12 13:51:41 +0200

    Swapped fapi dependency for mixins

--- src/main/java/com/nerjal/structureremover/StructureRemover.java
+++ src/main/java/com/nerjal/structureremover/StructureRemover.java
@@ -1,8 +1,6 @@
 package com.nerjal.structureremover;
 
 import it.unimi.dsi.fastutil.longs.LongSet;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureStart;
 import net.minecraft.util.math.BlockPos;
@@ -14,15 +12,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 
-public class StructureRemover implements ModInitializer {
+public class StructureRemover {
     static final Logger LOGGER = LogManager.getLogger("StructureRemover");
-
-    @Override
-    public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            StructureRemoverCommand.register(dispatcher);
-        });
-    }
 
     public static boolean removeStructure(@NotNull ServerWorld world, BlockPos pos, Structure structure) {
         Map<Structure, LongSet> structures = world.getStructureAccessor().getStructureReferences(pos);
